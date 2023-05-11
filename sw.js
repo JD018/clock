1f9e9d55b5a448f5cce441356ba98b8fa9345ebb
Caden Skidmore <c1641758c@gmail.com>
2023-05-11 13:42:49 -0400

    On Hold

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
-const urlsToCache = ["/"];
-self.addEventListener("install", event => {
-   event.waitUntil(
-      caches.open("pwa-assets")
-      .then(cache => {
-         return cache.addAll(urlsToCache);
-      });
-   );
-});
+//pain
