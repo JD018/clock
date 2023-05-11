6fd183490a3019a14d713842c81d72f701a93aa6
Caden Skidmore <c1641758c@gmail.com>
2023-05-11 13:15:26 -0400

    test

--- sw.js
+++ sw.js
@@ -1,16 +1,8 @@
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('asset_cache').then(function (cache) {
-      return cache.addAll([
-        '/images',
-        '/scripts',
-        '/styles',
-        'index.html',
-        'manifest.json',
-        'settings.html',
-        'sw.js'
-        // etc.
-      ]);
-    }),
-  );
+caches.open("pwa-assets")
+.then(cache => {
+  cache.addAll(
+    [
+      "/"
+    ]
+    ); 
 });
