7e2d185edb66b4d21a10a9b1c872dfd80cfabe27
Caden Skidmore <c1641758c@gmail.com>
2023-05-11 09:12:37 -0400

    fixed more things

--- sw.js
+++ sw.js
@@ -2,7 +2,19 @@ const CACHE_NAME = 'cool-cache';
 
 // Add whichever assets you want to pre-cache here:
 const PRECACHE_ASSETS = [
-    '/'
+    'clock_shortcut.png',
+    'favicon.ico',
+    'icon.png',
+    'index.html',
+    'initiate.js',
+    'legacy.css',
+    'manifest.json',
+    'run.js',
+    'screenshot.png',
+    'settings.css',
+    'settings.html',
+    'sw.js',
+    'update.js'
 ]
 
 // Listener for the install event - pre-caches our assets list on service worker install.
