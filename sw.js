b1ee23d9e78e95307a2524f5050e0facc3fa11fb
Caden Skidmore <c1641758c@gmail.com>
2023-05-11 13:18:10 -0400

    major test

--- sw.js
+++ sw.js
@@ -2,7 +2,13 @@ caches.open("pwa-assets")
 .then(cache => {
   cache.addAll(
     [
-      "/"
+      "index.html",
+      "manifest.json",
+      "settings.html",
+      "sw.js",
+      "images",
+      "scripts",
+      "styles"
     ]
     ); 
 });
