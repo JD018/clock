2bdd3b7fea67609b818e21c1a18746016a30dc73
Caden Skidmore <c1641758c@gmail.com>
2023-05-11 13:50:46 -0400

    here we go

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-//pain
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = [
+    '/images/',
+    '/scripts/',
+    '/styles/',
+    'index.html',
+    'settings.html',
+]
+
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
