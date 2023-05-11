e46d511f9ff714c72a4be873f72a4e4802e959e1
Caden Skidmore <c1641758c@gmail.com>
2023-05-11 12:56:39 -0400

    commit

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-/* const CACHE = "pwabuilder-offline-page";
+const CACHE = "pwabuilder-offline-page";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'); */
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "offline.html";
 
-/* self.addEventListener("message", (event) => {
+self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
-}); */
+});
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
@@ -20,7 +20,7 @@ self.addEventListener('install', async (event) => {
   );
 });
 
-/* if (workbox.navigationPreload.isSupported()) {
+if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
@@ -51,4 +51,4 @@ self.addEventListener('fetch', (event) => {
       }
     })());
   }
-}); */
+});
